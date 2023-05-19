4673bcf1a5fb3a0e9c7d0683b1b06454df563179
Soner Paycı <sonerpayci@gmail.com>
2023-05-19 21:34:43 +0300

    Data Binding with complex collection of objects through Mvvm pattern

--- MvvmDemo/Mvvm/ViewModels/PeopleViewModel.cs
+++ MvvmDemo/Mvvm/ViewModels/PeopleViewModel.cs
@@ -9,18 +9,18 @@ namespace MvvmDemo.Mvvm.ViewModels
 {
     public class PeopleViewModel
     {
-        public List<string> People { get; set; }
+        public List<Person> People { get; set; } = new();
 
         public PeopleViewModel()
         {
-            People = new List<string>
+            People = new List<Person>
             {
-                "Soner",
-                "Messi",
-                "Aguero",
-                "Ronaldo",
-                "David",
-                "Bale",
+                new Person{Name = "Soner", Age = 28, BirthDate = new DateTime(1995,1,1), Married = false, Weight = 90, LunchTime = new TimeSpan(12,0,0)},
+                new Person{Name = "Messi", Age = 30, BirthDate = new DateTime(1993,1,1), Married = true, Weight = 75, LunchTime = new TimeSpan(13,0,0)},
+                new Person{Name = "Aguero", Age = 35, BirthDate = new DateTime(1988,1,1), Married = false, Weight = 78, LunchTime = new TimeSpan(14,0,0)},
+                new Person{Name = "Ronaldo", Age = 32, BirthDate = new DateTime(1991,1,1), Married = true, Weight = 80, LunchTime = new TimeSpan(15,0,0)},
+                new Person{Name = "David", Age = 40, BirthDate = new DateTime(1983,1,1), Married = true, Weight = 78, LunchTime = new TimeSpan(16,0,0)},
+                new Person{Name = "Bale", Age = 41, BirthDate = new DateTime(1982,1,1), Married = true, Weight = 91, LunchTime = new TimeSpan(17,0,0)},
             };
         }
     }
